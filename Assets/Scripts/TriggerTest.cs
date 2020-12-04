74eac5af536fdc056da5bac4fc7888d23c2e3449
Oliver <oliverjamarie@gmail.com>
2020-12-04 22:43:40 +0100

    Trigger Testing

---
+++ Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour
+{
+    Collider collide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        collide = GetComponent<Collider>();
+        Debug.Log(collide.isTrigger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        Debug.Log("triggered");
+    }
+}
