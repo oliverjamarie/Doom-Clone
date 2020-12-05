2675fa6829e8f09e684ac17235a20bf1b7850b7e
Oliver <oliverjamarie@gmail.com>
2020-12-05 14:32:32 +0100

    Package Imports and Level Updates

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform goal;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        
+    }
+
+    void Update(){
+        agent.destination = goal.position;
+    }
+
+}
