2e92a517ce057b3cee153c435d5e09d2dfb67fe3
Oliver <oliverjamarie@gmail.com>
2020-12-05 18:32:16 +0100

    Player Movement Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController control;
+    public Animator animate;
 
     public float speed = 12f;
     public float sprintModifyer = 1.5f;
     public float gravity = -9.81f;
     public float groundCheckRadius = 0.4f;
-    public float jumpHeight = 3f;
+    public float jumpHeight;
     //public Transform groundCheck;
     //public LayerMask groundMask;
+
+    void Start (){
+        animate = GetComponent<Animator>();
+    }
 
     
     Vector3 velocity;
@@ -21,12 +26,13 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         isGrounded = control.isGrounded;
 
         bool onIncline = isOnIncline();
 
         if (isGrounded && velocity.y < 0){
-            velocity.y = -2f;
+            velocity.y = 0f;
         }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical"); 
