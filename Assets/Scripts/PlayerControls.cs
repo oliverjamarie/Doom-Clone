f702cb46ef0fe6abca5a65f08e0ede000bb4fca0
Oliver Marie <oliverjamarie@gmail.com>
2020-12-07 17:49:34 +0100

    Create PlayerControls.cs

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    Animator animator;
+    bool unskippableAnimationPlaying;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        unskippableAnimationPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+
+}
