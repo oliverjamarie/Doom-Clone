83b0e084675fdc3449efda88c074c648917f95f7
Oliver Marie <oliverjamarie@gmail.com>
2020-12-08 15:48:59 +0100

    CharactAnimationHandler

---
+++ Assets/Scripts/CharacterAnimationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationHandler : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump")){
+             anim.Play("BasicMotions@Jump01 [RootMotion]", 0,0);
+        }
+           
+    }
+}
