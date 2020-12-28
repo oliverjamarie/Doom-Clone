7c71ee6b87d1e5cdc4fdb455b615a7d6bd99b1d7
Oliver Marie <oliverjamarie@gmail.com>
2020-12-28 12:50:46 +0100

    Create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<Transform> SpawnPoints;
+    public List<GameObject> EnemyTypes;
+    public int numEnemiesToSpawn;
+
+    public GameObject player;
+
+    List<GameObject> characters;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characters = new List<GameObject>();
+
+        for (int i = 0; i < numEnemiesToSpawn; i ++){
+            int randSpawn = (int) (Random.value * 100) % SpawnPoints.Capacity;
+            int randEnemy = (int) (Random.value * 100) % EnemyTypes.Capacity;
+
+            GameObject enemy = EnemyTypes[randEnemy];
+            
+            
+
+            Instantiate(enemy,SpawnPoints[randSpawn]);
+
+            enemy.GetComponent<EnemyMovement>().playerObject = player;
+            
+
+            characters.Add(enemy);
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/* 
+        List<GameObject> toRemove = new List<GameObject>();
+
+        foreach (GameObject obj in characters)
+        {
+            EnemyInfo enemy = obj.GetComponent<EnemyInfo>();
+            
+            
+            if (enemy.isAlive == false){
+
+                print("Enemy is Dead");
+
+                Destroy(obj);
+                toRemove.Add(obj);
+            }
+        }
+
+        foreach (GameObject obj in toRemove)
+        {
+            characters.Remove(obj);
+        } */
+    }
+}
