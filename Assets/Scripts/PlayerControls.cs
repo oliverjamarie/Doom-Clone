b9ffb7f516d858ad6c7246955c27463e2a07d425
Oliver Marie <oliverjamarie@gmail.com>
2020-12-28 12:51:29 +0100

    Update PlayerControls.cs

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    Animator animator;
-    bool unskippableAnimationPlaying;
+
+    public Camera camera;
+    public LayerMask layerMask;
+    public GameObject gun; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponentInChildren<Animator>();
-        unskippableAnimationPlaying = false;
+        camera = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1")){
+            Transform camTransform = camera.transform;
+
+            GunInfo gunInfo = gun.GetComponent<GunInfo>();
+
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition +( Random.insideUnitSphere * gunInfo.accuracy));
+
+            if(Physics.Raycast(ray, out hit)){
+                EnemyInfo enemy = hit.transform.GetComponent<EnemyInfo>();
+
+                if (enemy != null ){
+                    enemy.takeDamage(gunInfo.damage);
+                }
+            }
+        }
 
     }
 
