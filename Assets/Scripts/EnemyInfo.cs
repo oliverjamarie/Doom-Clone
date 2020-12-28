d774664c1911e59ca8ce96745c3ceb8edb8bacd9
Oliver Marie <oliverjamarie@gmail.com>
2020-12-28 12:51:12 +0100

    Create EnemyInfo.cs

---
+++ Assets/Scripts/EnemyInfo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour
+{
+
+    public float baseHealth = 100f;
+    float currHealth;
+
+
+    public bool isAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currHealth = baseHealth;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currHealth <= 0f){
+            isAlive = false;
+        }
+    }
+
+
+    // handles the unit taking damage
+    // returns true if the unit is still alive after taking damage
+    // returns false if it dies from taking damage
+    public bool takeDamage(float damage){
+        if (isAlive){
+            currHealth -= damage;
+            print (currHealth);
+
+            if (currHealth <= 0f){
+                isAlive = false;
+                Destroy(gameObject);
+                return false;
+            }
+            return true;
+        }
+        return false;
+
+    }
+
+}
