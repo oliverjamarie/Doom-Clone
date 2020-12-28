a7f9f1d85098f7583de5a21f7197181580c91f9c
Oliver Marie <oliverjamarie@gmail.com>
2020-12-28 12:48:06 +0100

    Update PlayerInfo.cs

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerInfo : MonoBehaviour
 {
+
+    public float health = 100f;
+
+    public Transform targetFd, 
+                    targetBk,
+                    targetL,
+                    targetR,
+                    targetCurr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetCurr = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        targetCurr = transform;
+    }
+
+    public Transform getTargetFd(){
+        return targetFd;
+    }
+
+    public Transform getTargetBk(){
+        return targetBk;
+    }
+
+    public Transform getTargetR(){
+        return targetR;
+    }
+
+    public Transform getTargetL(){
+        return targetL;
+    }
+
+    public Transform getTargetCurr(){
+        return targetCurr;
     }
 }
